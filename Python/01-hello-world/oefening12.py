e87a386c84dfd29cd40d1b70bda0792a8f92cfc6
Mirkan-23 <mirkan070@outlook.com>
2026-08-16 21:48:49 +0200

    Oefening 12

---
+++ Python/01-hello-world/oefening12.py
@@ -0,0 +1,11 @@
+string1 = "De wind, "
+string2 = "die ons tot dan toe met verbazingwekkende snelheid had voortgedreven, "
+string3 = "nam bij zonsondergang af tot een lichte bries; "
+string4 = "de zachte lucht beroerde het water en "
+string5 = "veroorzaakte een aangename beweging tussen de bomen toen we de kust naderden, "
+string6 = "waar de heerlijke geur van bloemen en hooi opsteeg."
+
+# Define message below:
+bericht = (string1 + string2 + string3 + string4 + string5 + string6)
+
+print(bericht)
