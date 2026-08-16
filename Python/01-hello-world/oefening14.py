c0d6b7f2e714bcefaf26df5ecf0cc2ffe02a39c2
Mirkan-23 <mirkan070@outlook.com>
2026-08-16 21:54:20 +0200

    Oefening 14

---
+++ Python/01-hello-world/oefening14.py
@@ -0,0 +1,6 @@
+aan_jou = """
+Vreemdeling, als u mij toevallig tegenkomt en met mij wilt spreken,
+ waarom zou u dan niet met mij spreken?
+En waarom zou ik niet met u spreken?
+"""
+print(aan_jou)
