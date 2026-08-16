69552877fc5e70f42ba71982f1ea3e30259cefd1
Mirkan-23 <mirkan070@outlook.com>
2026-08-16 22:05:04 +0200

    Oefening 15

---
+++ Python/01-hello-world/oefening15.py
@@ -0,0 +1,7 @@
+mijn_leeftijd = 22
+helft_leeftijd = mijn_leeftijd / 2
+groet = "Silav"
+naam = "S-Sentinel"
+groet_met_naam = (groet + ", mijn naam is " + naam)
+
+print(groet_met_naam, "en ik ben", mijn_leeftijd, "jaar oud.", "De helft van mijn leeftijd is", helft_leeftijd)
