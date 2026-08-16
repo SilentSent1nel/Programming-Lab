7cab09f71475d228ca4e8f8cbba5c3d62c757d26
Mirkan-23 <mirkan070@outlook.com>
2026-08-16 21:28:25 +0200

    Oefening 10

---
+++ Python/01-hello-world/oefening10.py
@@ -0,0 +1,12 @@
+# Bereken de macht voor:
+# 6x6
+print(6**2)
+
+# 7x7
+print(7**2)
+
+# 8x8
+print(8**2)
+
+# Hoeveel vierkanten voor 6 mensen is er nodig om 6 dekens te maken dat elke 6x6 zijn?
+print(6**3 * 6)
