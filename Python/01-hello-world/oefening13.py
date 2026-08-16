065dcf0b50330fe943d607b02a00399e442c5516
Mirkan-23 <mirkan070@outlook.com>
2026-08-16 21:52:05 +0200

    Oefening 13

---
+++ Python/01-hello-world/oefening13.py
@@ -0,0 +1,15 @@
+totale_prijs = 0
+
+nieuwe_sneakers = 50.00
+
+totale_prijs += nieuwe_sneakers
+
+leuke_sweater = 39.00
+
+totale_prijs += leuke_sweater
+
+leuke_boeken = 20.00
+# Update total_price here:
+totale_prijs += leuke_boeken
+
+print("De totale prijs is", totale_prijs)
