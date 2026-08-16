481d262a87b2a6580a00a213493bf3e6b1806597
Mirkan-23 <mirkan070@outlook.com>
2026-08-16 22:33:48 +0200

    Project blok letters

---
+++ Python/01-hello-world/project_blok-letters.py
@@ -0,0 +1,11 @@
+# Mijn naam: S-Sentinel
+# Leuke feit over mijzelf: 01000100 01100001 01110100 00100000 01100010 01101100 01101001 01101010 01100110 01110100 00100000 01100101 01100101 01101110 00100000 01100111 01100101 01101000 01100101 01101001 01101101 00100000 00111011 00101001
+
+# ASCII art voor mijn naam (S-SENTINEL)
+print(" SSS                     SSS     EEEEE    N   N    TTTTT    IIIII    N   N    EEEEE    N   N    EEEEE    L")
+print("S   S                   S   S    E        NN  N      T        I      NN  N    E        NN  N    E        L")
+print("S                       S        E        N N N      T        I      N N N    E        N N N    E        L")
+print(" SSS     ----------      SSS     EEEEE    N  NN      T        I      N  NN    EEEEE    N  NN    EEEEE    L")
+print("    S                       S    E        N   N      T        I      N   N    E        N   N    E        L")
+print("S   S                   S   S    E        N   N      T        I      N   N    E        N   N    E        L")
+print(" SSS                     SSS     EEEEE    N   N      T      IIIII    N   N    EEEEE    N   N    EEEEE    LLLLL")
