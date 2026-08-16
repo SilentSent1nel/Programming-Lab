f923d463d91ea5566856ba680651f90641d595f3
Mirkan-23 <mirkan070@outlook.com>
2026-08-16 21:08:20 +0200

    Oefening 07

---
+++ Python/01-hello-world/oefening7.py
@@ -0,0 +1,7 @@
+# Definieer de integer-variabelen voor release en runtime hieronder:
+release_year = 2021
+runtime = 160
+
+
+# Definieer hieronder de float-variabele rating_out_of_10:
+rating_out_of_10 = 8.3
