e4f601973c0ade10227b387631e907005bd0bec6
Mirkan-23 <mirkan070@outlook.com>
2026-08-16 21:40:38 +0200

    Oefening 11

---
+++ Python/01-hello-world/oefening11.py
@@ -0,0 +1,9 @@
+eerste_bestelling_restant = 269 % 10
+print(eerste_bestelling_restant)
+
+eerste_bestelling_coupon = "no"
+
+tweede_bestelling_restant = 270 % 10
+print(tweede_bestelling_restant)
+
+tweede_bestelling_coupon = "yes"
