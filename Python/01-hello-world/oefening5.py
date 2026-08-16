fc938fc38d678b216673cfde9bbb6a0de328a881
Mirkan-23 <mirkan070@outlook.com>
2026-08-16 20:55:21 +0200

    Oefening 05

---
+++ Python/01-hello-world/oefening5.py
@@ -0,0 +1,20 @@
+# We hebben de variabele "maaltijd" hier ingesteld op de naam van het gerecht dat we als ontbijt hebben gegeten.
+maaltijd = "Een engelse muffin"
+
+# Ontbijt uitprinten
+print("Ontbijt:")
+print(maaltijd)
+
+# Wijzig de maaltijd nu naar lunch!
+maaltijd = "Tonijnsalade"
+
+# Lunch uitprinten
+print("Lunch:")
+print(maaltijd)
+
+# Wijzig nu "maaltijd" naar avondeten
+maaltijd = "Verse ossenhaas"
+
+# Avondeten uitprinten
+print("Avondeten:")
+print(maaltijd)
