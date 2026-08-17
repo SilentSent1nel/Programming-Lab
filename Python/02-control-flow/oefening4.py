84492e23c55206997131303928cf38fc2ee0dc17
Mirkan-23 <mirkan070@outlook.com>
2026-08-17 20:28:06 +0200

    Oefening 04

---
+++ Python/02-control-flow/oefening4.py
@@ -0,0 +1,6 @@
+gebruikers_naam = "Dave"
+
+if gebruikers_naam == "Dave":
+    print("Ga van mijn computer vandaan Dave!")
+if gebruikers_naam == "angela_catlady_87":
+    print("Ik weet dat jij het bent Dave! Ga weg!")
