2ef8d5f291d37d7c362cd922f7c5be09e19b2cf8
Mirkan-23 <mirkan070@outlook.com>
2026-08-17 20:34:39 +0200

    Oefening 06

---
+++ Python/02-control-flow/oefening6.py
@@ -0,0 +1,9 @@
+statement_1 = (2 + 2 + 2 >= 6) and (-1 * -1 < 0)
+
+statement_2 = (4 * 2 <= 8) and (7 - 1 == 6)
+
+punten = 120
+gpa = 3.4
+
+if punten >= 120 and gpa >= 2.0:
+  print("Je voldoet aan de eisen om te afstuderen!")
