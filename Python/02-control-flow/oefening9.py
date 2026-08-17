ba4015abeb69d10a5055bb2b85474eaab2e74a8e
Mirkan-23 <mirkan070@outlook.com>
2026-08-17 21:05:14 +0200

    Oefening 09

---
+++ Python/02-control-flow/oefening9.py
@@ -0,0 +1,7 @@
+puntne = 120
+gpa = 1.9
+
+if (puntne >= 120) and (gpa >= 2.0):
+  print("Je voldoet aan de eisen om te afstuderen!")
+else:
+  print("Je voldoet niet aan de eisen om te afstuderen.")
