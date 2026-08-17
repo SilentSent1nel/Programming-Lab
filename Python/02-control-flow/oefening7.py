c0240bfebd69cd274125cedce003f48dfa70ec50
Mirkan-23 <mirkan070@outlook.com>
2026-08-17 20:50:14 +0200

    Oefening 07

---
+++ Python/02-control-flow/oefening7.py
@@ -0,0 +1,9 @@
+statement_1 = (2 - 1 > 3) or (-5 * 2 == -10)
+
+statement_2 = (9 + 5 <= 15) or (7 != 4 + 3)
+
+punten = 118
+gpa = 2.0
+
+if punten >= 120 or gpa >= 2.0:
+    print("Je voldoet tenminste aan één van de eisen")
