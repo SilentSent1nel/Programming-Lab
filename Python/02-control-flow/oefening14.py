6adf624a9a3912ae326fb3f2f0fe260131fb41ff
Mirkan-23 <mirkan070@outlook.com>
2026-08-18 22:11:47 +0200

    Oefening 14

---
+++ Python/02-control-flow/oefening14.py
@@ -0,0 +1,16 @@
+# Gelukskoekje Program 🥠
+
+import random
+
+fortune = random.randint(0, 4)
+
+if fortune == 0:
+  print("Moge je ooit CO2-belastend zijn")
+elif fortune == 1:  
+  print("Je hebt rijst tussen je tanden")
+elif fortune == 2:
+  print("Geen enkele sneeuwvlok voelt zich verantwoordelijk voor een lawine")
+elif fortune == 3:
+  print("Je kunt alleen de punten verbinden als je achteruit kijkt")
+elif fortune == 4:
+  print("Het geluk dat je zoekt zit in een andere koekje")
