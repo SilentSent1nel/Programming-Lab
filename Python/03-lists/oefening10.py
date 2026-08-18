fe21881af1f3eed447c76ae0665a6a3580ea116f
Mirkan-23 <mirkan070@outlook.com>
2026-08-18 23:23:09 +0200

    Oefening 10

---
+++ Python/03-lists/oefening10.py
@@ -0,0 +1,16 @@
+bestellijst = ["Selderijen", "Sinaasappelsappen", "Sinaasappels", "Platte broden"]
+print(bestellijst)
+
+bestellijst.remove("Platte broden")
+print(bestellijst)
+
+# Nieuwe supermarkt
+nieuwe_supermarkt_bestellijst = ["Sinaasappel", "Appel", "Mango", "Broccoli", "Mango"]
+print(nieuwe_supermarkt_bestellijst)
+
+nieuwe_supermarkt_bestellijst.remove("Mango")
+print(nieuwe_supermarkt_bestellijst)
+
+# Foutmelding omdat "Uien niet voorkomen in de bestellijst"
+# new_store_order_list.remove("Onions")
+# print(new_store_order_list)
