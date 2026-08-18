3d4e0d8153bc98fbbdafef8058caecef59df7d3b
Mirkan-23 <mirkan070@outlook.com>
2026-08-18 23:33:11 +0200

    Oefening 12

---
+++ Python/03-lists/oefening12.py
@@ -0,0 +1,8 @@
+klas_namen_toets = [["Jenny", 90], ["Alexus", 85.5], ["Sam", 83], ["Ellie", 101.5]]
+print(klas_namen_toets)
+
+sams_cijfer = klas_namen_toets[2][1]
+print(sams_cijfer)
+
+ellies_cijfer = klas_namen_toets[-1][-1]
+print(ellies_cijfer)
