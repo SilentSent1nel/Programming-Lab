2142ac0d8108c08e5035286050a9ab74607cbb00
Mirkan-23 <mirkan070@outlook.com>
2026-08-18 23:45:03 +0200

    Oefening 13

---
+++ Python/03-lists/oefening13.py
@@ -0,0 +1,8 @@
+nieuwe_klas = [["Kenny", "Amerikaans", 9], ["Tanya", "Oekraïens", 9], ["Madison", "Indiaas", 7]]
+print(nieuwe_klas)
+
+madison_naar_groep8 = nieuwe_klas[2][2] = 8
+print(nieuwe_klas)
+
+kenny_naam_veranderen = nieuwe_klas[-3][-3] = "Ken"
+print(nieuwe_klas)
