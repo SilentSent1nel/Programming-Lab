45db54af69b4c7c5659a996a5fa630e014450184
Mirkan-23 <mirkan070@outlook.com>
2026-08-18 22:26:21 +0200

    Oefening 16

---
+++ Python/02-control-flow/oefening16.py
@@ -0,0 +1,20 @@
+# Oppervlaktecalculator 📏
+
+import math
+
+basis = 20
+hoogte = 30
+oppervlakte = basis * hoogte / 2
+
+print("The oppervlakte van de driehoek is", oppervlakte)
+
+lengte = 2
+breedte = 12
+oppervlakte = lengte * breedte
+
+print("De oppervlakte van de rechthoek is", oppervlakte)
+    
+radius = 36
+oppervlakte = math.pi * radius * radius
+    
+print("De oppervlakte van de cirkel is", oppervlakte)
