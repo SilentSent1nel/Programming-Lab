af06a4b5aec62740fec5e1b000f4212c388f7f73
Mirkan-23 <mirkan070@outlook.com>
2026-08-18 22:22:12 +0200

    Oefening 15

---
+++ Python/02-control-flow/oefening15.py
@@ -0,0 +1,23 @@
+# Wie wilt er een miljonair zijn 💰 
+
+score = 0
+
+keuze1 = 'Koffie'
+keuze2 = 'Thee'
+keuze3 = 'Cola'
+keuze4 = 'A&W'
+  
+print("Om zijn favoriete drankjes op bestelling te kunnen bestellen, liet LBJ vier knoppen in het Oval Office installeren met de opschriften 'Koffie', 'Thee', 'Cola' en wat nog meer?\n")
+
+print("A.", keuze1)
+print("B.", keuze2)
+print("C.", keuze3)
+print("D.", keuze4)
+  
+antwoord = 'a'
+
+if antwoord == 'A' or antwoord == 'a': 
+  score += 100
+  print("\nCorrect!")
+else:
+  print("\nNope, sorry!")
