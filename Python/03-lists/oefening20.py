ac003836d23878199cb4d85b59281b727950ab36
Mirkan-23 <mirkan070@outlook.com>
2026-08-19 01:59:00 +0200

    Oefening 20

---
+++ Python/03-lists/oefening20.py
@@ -0,0 +1,9 @@
+lange_lijst = [1, 5, 6, 7, -23, 69.5, True, "very", "long", "list", "that", "keeps", "going.", "Let's", "practice", "getting", "the", "length"]
+
+groot_bereik = range(2, 3000, 100)
+
+lengte_lange_lijst = len(lange_lijst)
+print(lengte_lange_lijst)
+
+lengte_groot_bereik = len(groot_bereik)
+print(lengte_groot_bereik)
