6d70a1ac0cced318948ef35622ec4653db7a24e4
Mirkan-23 <mirkan070@outlook.com>
2026-08-18 23:00:20 +0200

    Oefening 6

---
+++ Python/03-lists/oefening6.py
@@ -0,0 +1,7 @@
+bestellingen = ["madeliefje", "boterbloem", "leeuwenbek", "gardenia", "lelie"]
+
+# Maak hier nieuwe bestellingen:
+nieuwe_bestellingen = ["lila", "iris"]
+bestellingen_gecombineerd = bestellingen + nieuwe_bestellingen
+
+kapotte_prijzen = [5, 3, 4, 5, 4] + [4]
