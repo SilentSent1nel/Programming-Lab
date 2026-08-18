5c5e687bb774f62ac8c10da28da61d97917ba3c4
Mirkan-23 <mirkan070@outlook.com>
2026-08-18 23:11:58 +0200

    Oefening 8

---
+++ Python/03-lists/oefening8.py
@@ -0,0 +1,5 @@
+boodschappen_lijst = ["eiren", "boter", "melk", "komkommers", "sap", "graan"]
+laatste_element = boodschappen_lijst[-1]
+print(laatste_element)
+index5_element = boodschappen_lijst[5]
+print(index5_element)
