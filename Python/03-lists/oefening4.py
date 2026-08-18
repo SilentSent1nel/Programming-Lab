083357a1345d6dc632eff8a37917d9bd3ee52186
Mirkan-23 <mirkan070@outlook.com>
2026-08-18 22:45:26 +0200

    Oefening 4

---
+++ Python/03-lists/oefening4.py
@@ -0,0 +1,10 @@
+voorbeeld_lijst = [1, 2, 3, 4]
+print(voorbeeld_lijst)
+
+# Append Gebruik
+voorbeeld_lijst.append(5)
+print(voorbeeld_lijst)
+
+# Remove Gebruik
+voorbeeld_lijst.remove(5)
+print(voorbeeld_lijst)
