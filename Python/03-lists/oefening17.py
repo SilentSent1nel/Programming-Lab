32bc9299da7e7edee99cf21b77b064f42cc73965
Mirkan-23 <mirkan070@outlook.com>
2026-08-19 01:41:15 +0200

    Oefening 17

---
+++ Python/03-lists/oefening17.py
@@ -0,0 +1,11 @@
+data_wetenschap_onderwerpen = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_wetenschap_onderwerpen)
+
+
+verwijder_p3 = data_wetenschap_onderwerpen.pop(-1)
+print(data_wetenschap_onderwerpen)
+print(verwijder_p3)
+
+verwijder_algoritmes = data_wetenschap_onderwerpen.pop(-2)
+print(data_wetenschap_onderwerpen)
+print(verwijder_algoritmes)
