2f56319a71e890b31735955fe288285d2f150008
Mirkan-23 <mirkan070@outlook.com>
2026-08-19 22:54:48 +0200

    Oefening 7

---
+++ Python/04-loops/oefening7.py
@@ -0,0 +1,9 @@
+hondenrassen_beschikbaar_voor_adoptie = ["Franse bulldog", "Dalmatiër", "Shih Tzu", "poedel", "collie"]
+hondenras_dat_ik_wil = "Dalmatiër"
+
+for hondenras in hondenrassen_beschikbaar_voor_adoptie:
+    print(hondenras)
+
+    if hondenras == hondenras_dat_ik_wil:
+        print("Ze hebben de hond die ik wil!")
+        break
