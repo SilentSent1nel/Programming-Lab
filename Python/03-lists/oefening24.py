ad38aa824856203834cae6d2881274a97320d6d4
Mirkan-23 <mirkan070@outlook.com>
2026-08-19 02:26:40 +0200

    Oefening 24

---
+++ Python/03-lists/oefening24.py
@@ -0,0 +1,18 @@
+# CP 1 & 2
+adressen = ["221 B Baker St.", "42 Wallaby Way", "12 Grimmauld Place", "742 Evergreen Terrace", "1600 Pennsylvania Ave", "10 Downing St."]
+
+
+# CP 3
+namen = ["Ron", "Hermione", "Harry", "Albus", "Sirius"]
+namen.sort()
+
+
+# CP 4 & 5
+steden = ["London", "Paris", "Rome", "Los Angeles", "New York"]
+gesorteerde_steden = steden.sort(reverse=True)
+
+adressen.sort()
+print(adressen)
+
+print(gesorteerde_steden)
+print(steden)
