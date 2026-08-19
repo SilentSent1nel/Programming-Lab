dd9dfdbd9eae3b446ec16701b2713041610e865d
Mirkan-23 <mirkan070@outlook.com>
2026-08-19 23:12:59 +0200

    Oefening 11

---
+++ Python/04-loops/oefening11.py
@@ -0,0 +1,8 @@
+lengtes = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+mag_in_achtbaan = []
+
+for lengte in lengtes:
+    if lengte > 161:
+        mag_in_achtbaan.append(lengte)
+
+print(mag_in_achtbaan)
