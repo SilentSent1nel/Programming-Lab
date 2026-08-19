1618efaf246c620bb4ed7c87fa868334001a6d3a
Mirkan-23 <mirkan070@outlook.com>
2026-08-19 02:20:42 +0200

    Oefening 23

---
+++ Python/03-lists/oefening23.py
@@ -0,0 +1,4 @@
+stemmen = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+jake_stemmen = stemmen.count("Jake")
+print(jake_stemmen)
