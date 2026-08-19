c8c8b7acc1b127ba2a92cd7d2c403c5c9bff7198
Mirkan-23 <mirkan070@outlook.com>
2026-08-19 02:12:27 +0200

    Oefening 22

---
+++ Python/03-lists/oefening22.py
@@ -0,0 +1,7 @@
+koffer = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+ 
+laatste_twee_elementen = koffer[-2:]
+print(laatste_twee_elementen)
+
+laatste_drie_eraf = koffer[:-3]
+print(laatste_drie_eraf)
