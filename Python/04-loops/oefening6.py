1b76fef4c250dc671a872380957d470e7973021c
Mirkan-23 <mirkan070@outlook.com>
2026-08-19 22:46:10 +0200

    Oefening 6

---
+++ Python/04-loops/oefening6.py
@@ -0,0 +1,6 @@
+studenten_periode_A = ["Alex", "Briana", "Cheri", "Daniele"]
+studenten_periode_B = ["Dora", "Minerva", "Alexa", "Obie"]
+
+for student in studenten_periode_A:
+    studenten_periode_B.append(student)
+print(studenten_periode_B)
