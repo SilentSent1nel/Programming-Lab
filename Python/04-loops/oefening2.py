20de911c9b63561c2b01413010b1f0bbc082c3d7
Mirkan-23 <mirkan070@outlook.com>
2026-08-19 21:34:25 +0200

    Oefening 2

---
+++ Python/04-loops/oefening2.py
@@ -0,0 +1,11 @@
+bordspellen = ["Kolonisten van Catan", "Carcassonne", "Power Grid", "Agricola", "Scrabble"]
+
+sportspellen = ["voetbal", "hockey", "honkbal", "cricket"]
+
+for spel in bordspellen:
+    print(spel)
+
+print()
+
+for sporten in sportspellen:
+    print(sporten)
