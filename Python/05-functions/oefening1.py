a6fe5b69e3a147bfa9faa4ff8ebcbe09e04b45c3
Mirkan-23 <mirkan070@outlook.com>
2026-08-20 21:25:21 +0200

    Oefening 1

---
+++ Python/05-functions/oefening1.py
@@ -0,0 +1,4 @@
+def route_naar_timesSq():
+    print("Loop 4 minuten naar het metrostation 34th St Herald Square")
+    print("Neem de noordelijke N-, Q-, R- of W-trein 1 halte")
+    print("Stap uit bij de halte Times Square 42nd Street")
