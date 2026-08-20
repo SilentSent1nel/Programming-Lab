12c2e91230c81f472442b26e8f24e82a784ce4c2
Mirkan-23 <mirkan070@outlook.com>
2026-08-20 23:02:20 +0200

    Oefening 4

---
+++ Python/05-functions/oefening4.py
@@ -0,0 +1,6 @@
+locatie = "Central Park"
+
+def genereer_reisinstructies(locatie):
+    print("Je kunt het openbare metrosysteem gebruiken om naar", locatie, "te gaan")
+
+genereer_reisinstructies("Grand Central Station")
