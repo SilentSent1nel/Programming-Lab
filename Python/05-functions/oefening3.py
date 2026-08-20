5c42bb2a88d74e015f7856a71d4eb43fa623a694
Mirkan-23 <mirkan070@outlook.com>
2026-08-20 21:41:10 +0200

    Oefening 3

---
+++ Python/05-functions/oefening3.py
@@ -0,0 +1,7 @@
+print("Het weer voor je controleren!")
+
+def weer_controle():
+    print("Het ziet er buiten geweldig uit! Geniet van je reis.")
+    print("Vals alarm, het weer is veranderd! Er komt een onweersbui aan. Annuleer je plannen en blijf binnen.")
+
+weer_controle()
