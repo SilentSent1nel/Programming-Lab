fb1fa87972001bc4c9d6f4c5c03973c9302631bb
Mirkan-23 <mirkan070@outlook.com>
2026-08-21 19:19:38 +0200

    Oefening 10

---
+++ Python/05-functions/oefening10.py
@@ -0,0 +1,11 @@
+def populairste_toeristische_plaatsen_italie():
+  eerste = "Rome"
+  tweede = "Venetië"
+  derde = "Florence"
+
+  return eerste, tweede, derde
+
+populairste1, populairste2, populairste3 = populairste_toeristische_plaatsen_italie()
+print(populairste1)
+print(populairste2)
+print(populairste3)
