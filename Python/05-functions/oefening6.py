59a22a7c3407473941b7771a59de373a92d81391
Mirkan-23 <mirkan070@outlook.com>
2026-08-21 17:45:56 +0200

    Oefening 6

---
+++ Python/05-functions/oefening6.py
@@ -0,0 +1,5 @@
+def reisplanner(eerste_bestemming="IJsland", tweede_bestemming="India", laatste_bestemming="Duitsland"):
+    print("Zo ziet je reis eruit!")
+    print("Eerst stoppen we in", eerste_bestemming, "daarna", tweede_bestemming, "en als laatste", laatste_bestemming)
+
+reisplanner("Brooklyn", "Queens")
