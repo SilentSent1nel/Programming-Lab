66fb4cb11fe4e5ed7abf0565df8968808aaeaf42
Mirkan-23 <mirkan070@outlook.com>
2026-08-21 17:54:58 +0200

    Oefening 8

---
+++ Python/05-functions/oefening8.py
@@ -0,0 +1,12 @@
+# Deze functie print een vast aantal locaties dat we hebben.
+def print_aantal_locaties():
+  print("Er zijn 3 locaties")
+
+favoriete_locaties = "Parijs, Noorwegen, IJsland"
+
+# Deze functie print de favoriete locaties
+def toon_favoriete_locaties():
+  print("Je favoriete locaties zijn: " + favoriete_locaties)
+
+print_aantal_locaties()
+toon_favoriete_locaties()
