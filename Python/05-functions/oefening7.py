bb37bf314ea57c8aa437becf356a69e678f8bc46
Mirkan-23 <mirkan070@outlook.com>
2026-08-21 17:49:55 +0200

    Oefening 7

---
+++ Python/05-functions/oefening7.py
@@ -0,0 +1,13 @@
+tshirt_prijs = 9.75
+shorts_prijs = 15.50
+mok_prijs = 5.99
+poster_prijs = 2.00
+
+max_prijs = max(tshirt_prijs, shorts_prijs, mok_prijs, poster_prijs)
+print(max_prijs)
+
+min_prijs = min(tshirt_prijs, shorts_prijs, mok_prijs, poster_prijs)
+print(min_prijs)
+
+afgeronde_prijs = round(tshirt_prijs, 1)
+print(afgeronde_prijs)
