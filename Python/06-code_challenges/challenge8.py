48fbe72748a972420fe709579a8eefdfca4073f3
Mirkan-23 <mirkan070@outlook.com>
2026-08-23 17:43:41 +0200

    Challenge 8

---
+++ Python/06-code_challenges/challenge8.py
@@ -0,0 +1,11 @@
+def altijd_false(num):
+    if (num > 0 and num < 0):
+        return True
+    else:
+        return False
+
+# Zou "False" moeten printen
+print(altijd_false(1))
+
+# Zou "False" moeten printen
+print(altijd_false(2))
