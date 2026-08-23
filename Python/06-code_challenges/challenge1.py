684dc3877861aa48e441529084d894afa32bb6bb
Mirkan-23 <mirkan070@outlook.com>
2026-08-23 16:56:10 +0200

    Challenge 1

---
+++ Python/06-code_challenges/challenge1.py
@@ -0,0 +1,14 @@
+"""
+Gegeven zijn de 2 nummers in "num1" en "num2". Als de som van "num1" en "num2" niet gelijk aan 10 is,
+wordt de variabele niet_tien omgezet naar "True", en anders wordt het "False"
+"""
+
+num1 = 6
+num2 = 3
+
+if num1 + num2 != 10:
+    niet_tien = True
+else:
+    niet_tien = False
+
+print(niet_tien)
