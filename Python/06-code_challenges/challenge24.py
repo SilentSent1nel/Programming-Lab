e92f8d84c577ce824c1603e511bea2fc2f511d02
Mirkan-23 <mirkan070@outlook.com>
2026-08-24 18:34:00 +0200

    Challenge 24

---
+++ Python/06-code_challenges/challenge24.py
@@ -0,0 +1,12 @@
+lijst1 = []
+lijst2 = []
+
+def same_values(lijst1, lijst2):
+    nieuwe_lst = []
+    for index in range(len(lijst1)):
+        if lijst1[index] == lijst2[index]:
+            nieuwe_lst.append(index)
+
+    return nieuwe_lst
+
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
