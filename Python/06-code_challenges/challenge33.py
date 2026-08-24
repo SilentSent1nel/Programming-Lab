3aabc52dc575307797478d33fd04aee6051153d8
Mirkan-23 <mirkan070@outlook.com>
2026-08-24 19:31:38 +0200

    Challenge 33

---
+++ Python/06-code_challenges/challenge33.py
@@ -0,0 +1,7 @@
+def introduction(first_name, last_name):
+    return last_name + ', ' + first_name + ' ' + last_name
+
+print(introduction("Kawa", "Berxwedan"))
+print(introduction("Jiyan", "Şêrko"))
+print(introduction("Agrîn", "Welat"))
+print(introduction("Ronî", "Çîya"))
