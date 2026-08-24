fc36c58a94ef02b54d1e4050f4455de235b475da
Mirkan-23 <mirkan070@outlook.com>
2026-08-24 18:34:29 +0200

    Challenge 24 - Functie naam gewijzigd

--- Python/06-code_challenges/challenge24.py
+++ Python/06-code_challenges/challenge24.py
@@ -1,7 +1,7 @@
 lijst1 = []
 lijst2 = []
 
-def same_values(lijst1, lijst2):
+def zelfde_waarden(lijst1, lijst2):
     nieuwe_lst = []
     for index in range(len(lijst1)):
         if lijst1[index] == lijst2[index]:
@@ -9,4 +9,4 @@ def same_values(lijst1, lijst2):
 
     return nieuwe_lst
 
-print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+print(zelfde_waarden([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
