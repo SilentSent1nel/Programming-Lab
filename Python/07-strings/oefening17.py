e38d8b2fa55a1e9b6c324cc2f953a1b4abadb34c
Mirkan-23 <mirkan070@outlook.com>
2026-08-26 22:21:18 +0200

    Oefening 17

---
+++ Python/07-strings/oefening17.py
@@ -0,0 +1,6 @@
+winter_bomen_regels = ['Alle ingewikkelde details', 'van het aankleden en', 'het uitkleden zijn voltooid!', 'Een vloeibare maan', 'beweegt zachtjes tussen', 'de lange takken.', 'Zo hun knoppen voorbereid', 'op een zekere winter', 'staan ​​de wijze bomen', 'slapend in de kou.']
+
+for regel in winter_bomen_regels:
+    winter_bomen_volledig = '\n'.join(winter_bomen_regels)
+
+print(winter_bomen_volledig)
