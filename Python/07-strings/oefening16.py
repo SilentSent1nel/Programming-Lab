4f8effb98a373e56e0337d774cd716af127ab789
Mirkan-23 <mirkan070@outlook.com>
2026-08-26 22:06:08 +0200

    Oefening 16

---
+++ Python/07-strings/oefening16.py
@@ -0,0 +1,4 @@
+maaiers_regel_een_woorden = ["Zwart", "maaiers", "met", "het", "geluid", "van", "staal", "op", "stenen"]
+
+maaiers_regel_een = ' '.join(maaiers_regel_een_woorden)
+print(maaiers_regel_een)
