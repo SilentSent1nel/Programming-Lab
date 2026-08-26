b5c3758b365643facbd54c3e4da451c295833ce5
Mirkan-23 <mirkan070@outlook.com>
2026-08-26 21:57:52 +0200

    Oefening 15

---
+++ Python/07-strings/oefening15.py
@@ -0,0 +1,21 @@
+lente_storm_tekst = \
+"""De hemel heeft zijn bitterheid opgegeven.
+Uit de duisternis verandert de dag
+de hele dag door
+regent het onophoudelijk
+alsof het nooit zal ophouden.
+Toch houdt de sneeuw
+de grond in zijn greep.
+Maar water, water
+uit duizend stroompjes!
+Het verzamelt zich snel,
+bevlekt met zwart
+baant zich een weg
+door het groene ijs in de goten.
+Druppel na druppel valt het
+van de verdorde grasstengels
+van de overhangende berm."""
+
+
+lente_storm_regels = lente_storm_tekst.split("\n")
+print(lente_storm_regels)
