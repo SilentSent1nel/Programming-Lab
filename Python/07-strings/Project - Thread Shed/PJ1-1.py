850b070b405da4471139715818e5251b337eef8e
Mirkan-23 <mirkan070@outlook.com>
2026-08-27 00:41:46 +0200

    PJ1-1 - Engelstalige kleuren in dagelijkse_verkoop naar Nederlandstalig namen gewijzigd

--- Python/07-strings/Project - Thread Shed/PJ1-1.py
+++ Python/07-strings/Project - Thread Shed/PJ1-1.py
@@ -1,109 +1,109 @@
 dagelijkse_verkoop = \
-"""Edith Mcbride   ;,;$1.21   ;,;   white ;,; 
+"""Edith Mcbride   ;,;$1.21   ;,;   wit ;,; 
 09/15/17   ,Herbert Tran   ;,;   $7.29;,; 
-white&blue;,;   09/15/17 ,Paul Clarke ;,;$12.52 
-;,;   white&blue ;,; 09/15/17 ,Lucille Caldwell   
-;,;   $5.13   ;,; white   ;,; 09/15/17,
-Eduardo George   ;,;$20.39;,; white&yellow 
+wit&blauw;,;   09/15/17 ,Paul Clarke ;,;$12.52 
+;,;   wit&blauw ;,; 09/15/17 ,Lucille Caldwell   
+;,;   $5.13   ;,; wit   ;,; 09/15/17,
+Eduardo George   ;,;$20.39;,; wit&geel 
 ;,;09/15/17   ,   Danny Mclaughlin;,;$30.82;,;   
-purple ;,;09/15/17 ,Stacy Vargas;,; $1.85   ;,; 
-purple&yellow ;,;09/15/17,   Shaun Brock;,; 
-$17.98;,;purple&yellow ;,; 09/15/17 , 
-Erick Harper ;,;$17.41;,; blue ;,; 09/15/17, 
-Michelle Howell ;,;$28.59;,; blue;,;   09/15/17   , 
-Carroll Boyd;,; $14.51;,;   purple&blue   ;,;   
+paars ;,;09/15/17 ,Stacy Vargas;,; $1.85   ;,; 
+paars&geel ;,;09/15/17,   Shaun Brock;,; 
+$17.98;,;paars&geel ;,; 09/15/17 , 
+Erick Harper ;,;$17.41;,; blauw ;,; 09/15/17, 
+Michelle Howell ;,;$28.59;,; blauw;,;   09/15/17   , 
+Carroll Boyd;,; $14.51;,;   paars&blauw   ;,;   
 09/15/17   , Teresa Carter   ;,; $19.64 ;,; 
-white;,;09/15/17   ,   Jacob Kennedy ;,; $11.40   
-;,; white&red   ;,; 09/15/17, Craig Chambers;,; 
-$8.79 ;,; white&blue&red   ;,;09/15/17   , Peggy Bell;,; $8.65 ;,;blue   ;,; 09/15/17,   Kenneth Cunningham ;,;   $10.53;,;   green&blue   ;,; 
+wit;,;09/15/17   ,   Jacob Kennedy ;,; $11.40   
+;,; wit&rood   ;,; 09/15/17, Craig Chambers;,; 
+$8.79 ;,; wit&blauw&rood   ;,;09/15/17   , Peggy Bell;,; $8.65 ;,;blauw   ;,; 09/15/17,   Kenneth Cunningham ;,;   $10.53;,;   groen&blauw   ;,; 
 09/15/17   ,   Marvin Morgan;,;   $16.49;,; 
-green&blue&red   ;,;   09/15/17 ,Marjorie Russell 
-;,; $6.55 ;,;   green&blue&red;,;   09/15/17 ,
-Israel Cummings;,;   $11.86   ;,;black;,;  
+groen&blauw&rood   ;,;   09/15/17 ,Marjorie Russell 
+;,; $6.55 ;,;   groen&blauw&rood;,;   09/15/17 ,
+Israel Cummings;,;   $11.86   ;,;zwart;,;  
 09/15/17,   June Doyle   ;,;   $22.29 ;,;  
-black&yellow ;,;09/15/17 , Jaime Buchanan   ;,;   
-$8.35;,;   white&black&yellow   ;,;   09/15/17,   
-Rhonda Farmer;,;$2.91 ;,;   white&black&yellow   
-;,;09/15/17, Darren Mckenzie ;,;$22.94;,;green 
-;,;09/15/17,Rufus Malone;,;$4.70   ;,; green&yellow 
+zwart&geel ;,;09/15/17 , Jaime Buchanan   ;,;   
+$8.35;,;   wit&zwart&geel   ;,;   09/15/17,   
+Rhonda Farmer;,;$2.91 ;,;   wit&zwart&geel   
+;,;09/15/17, Darren Mckenzie ;,;$22.94;,;groen 
+;,;09/15/17,Rufus Malone;,;$4.70   ;,; groen&geel 
 ;,; 09/15/17   ,Hubert Miles;,;   $3.59   
-;,;green&yellow&blue;,;   09/15/17   , Joseph Bridges  ;,;$5.66   ;,; green&yellow&purple&blue 
+;,;groen&geel&blauw;,;   09/15/17   , Joseph Bridges  ;,;$5.66   ;,; groen&geel&paars&blauw 
 ;,;   09/15/17 , Sergio Murphy   ;,;$17.51   ;,;   
-black   ;,;   09/15/17 , Audrey Ferguson ;,; 
-$5.54;,;black&blue   ;,;09/15/17 ,Edna Williams ;,; 
-$17.13;,; black&blue;,;   09/15/17,   Randy Fleming;,;   $21.13 ;,;black ;,;09/15/17 ,Elisa Hart;,; $0.35   ;,; black&purple;,;   09/15/17   ,
-Ernesto Hunt ;,; $13.91   ;,;   black&purple ;,;   
+zwart   ;,;   09/15/17 , Audrey Ferguson ;,; 
+$5.54;,;zwart&blauw   ;,;09/15/17 ,Edna Williams ;,; 
+$17.13;,; zwart&blauw;,;   09/15/17,   Randy Fleming;,;   $21.13 ;,;zwart ;,;09/15/17 ,Elisa Hart;,; $0.35   ;,; zwart&paars;,;   09/15/17   ,
+Ernesto Hunt ;,; $13.91   ;,;   zwart&paars ;,;   
 09/15/17,   Shannon Chavez   ;,;$19.26   ;,; 
-yellow;,; 09/15/17   , Sammy Cain;,; $5.45;,;   
-yellow&red ;,;09/15/17 ,   Steven Reeves ;,;$5.50   
-;,;   yellow;,;   09/15/17, Ruben Jones   ;,; 
-$14.56 ;,;   yellow&blue;,;09/15/17 , Essie Hansen;,;   $7.33   ;,;   yellow&blue&red
+geel;,; 09/15/17   , Sammy Cain;,; $5.45;,;   
+geel&rood ;,;09/15/17 ,   Steven Reeves ;,;$5.50   
+;,;   geel;,;   09/15/17, Ruben Jones   ;,; 
+$14.56 ;,;   geel&blauw;,;09/15/17 , Essie Hansen;,;   $7.33   ;,;   geel&blauw&rood
 ;,; 09/15/17   ,   Rene Hardy   ;,; $20.22   ;,; 
-black ;,;   09/15/17 ,   Lucy Snyder   ;,; $8.67   
-;,;black&red  ;,; 09/15/17 ,Dallas Obrien ;,;   
-$8.31;,;   black&red ;,;   09/15/17,   Stacey Payne 
-;,;   $15.70   ;,;   white&black&red ;,;09/15/17   
-,   Tanya Cox   ;,;   $6.74   ;,;yellow   ;,; 
+zwart ;,;   09/15/17 ,   Lucy Snyder   ;,; $8.67   
+;,;zwart&rood  ;,; 09/15/17 ,Dallas Obrien ;,;   
+$8.31;,;   zwart&rood ;,;   09/15/17,   Stacey Payne 
+;,;   $15.70   ;,;   wit&zwart&rood ;,;09/15/17   
+,   Tanya Cox   ;,;   $6.74   ;,;geel   ;,; 
 09/15/17 , Melody Moran ;,;   $30.84   
-;,;yellow&black;,;   09/15/17 , Louise Becker   ;,; 
-$12.31 ;,; green&yellow&black;,;   09/15/17 ,
-Ryan Webster;,;$2.94 ;,; yellow ;,; 09/15/17 
-,Justin Blake ;,; $22.46   ;,;white&yellow ;,;   
+;,;geel&zwart;,;   09/15/17 , Louise Becker   ;,; 
+$12.31 ;,; groen&geel&zwart;,;   09/15/17 ,
+Ryan Webster;,;$2.94 ;,; geel ;,; 09/15/17 
+,Justin Blake ;,; $22.46   ;,;wit&geel ;,;   
 09/15/17,   Beverly Baldwin ;,;   $6.60;,;   
-white&yellow&black ;,;09/15/17   ,   Dale Brady   
-;,;   $6.27 ;,; yellow   ;,;09/15/17 ,Guadalupe Potter ;,;$21.12   ;,; yellow;,; 09/15/17   , 
-Desiree Butler ;,;$2.10   ;,;white;,; 09/15/17  
-,Sonja Barnett ;,; $14.22 ;,;white&black;,;   
-09/15/17, Angelica Garza;,;$11.60;,;white&black   
+wit&geel&zwart ;,;09/15/17   ,   Dale Brady   
+;,;   $6.27 ;,; geel   ;,;09/15/17 ,Guadalupe Potter ;,;$21.12   ;,; geel;,; 09/15/17   , 
+Desiree Butler ;,;$2.10   ;,;wit;,; 09/15/17  
+,Sonja Barnett ;,; $14.22 ;,;wit&zwart;,;   
+09/15/17, Angelica Garza;,;$11.60;,;wit&zwart   
 ;,;   09/15/17   ,   Jamie Welch   ;,; $25.27   ;,; 
-white&black&red ;,;09/15/17   ,   Rex Hudson   
-;,;$8.26;,;   purple;,; 09/15/17 ,   Nadine Gibbs 
-;,;   $30.80 ;,;   purple&yellow   ;,; 09/15/17   , 
-Hannah Pratt;,;   $22.61   ;,;   purple&yellow   
+wit&zwart&rood ;,;09/15/17   ,   Rex Hudson   
+;,;$8.26;,;   paars;,; 09/15/17 ,   Nadine Gibbs 
+;,;   $30.80 ;,;   paars&geel   ;,; 09/15/17   , 
+Hannah Pratt;,;   $22.61   ;,;   paars&geel   
 ;,;09/15/17,Gayle Richards;,;$22.19 ;,; 
-green&purple&yellow ;,;09/15/17   ,Stanley Holland 
-;,; $7.47   ;,; red ;,; 09/15/17 , Anna Dean;,;$5.49 ;,; yellow&red ;,;   09/15/17   ,
-Terrance Saunders ;,;   $23.70  ;,;green&yellow&red 
+groen&paars&geel ;,;09/15/17   ,Stanley Holland 
+;,; $7.47   ;,; rood ;,; 09/15/17 , Anna Dean;,;$5.49 ;,; geel&rood ;,;   09/15/17   ,
+Terrance Saunders ;,;   $23.70  ;,;groen&geel&rood 
 ;,; 09/15/17 ,   Brandi Zimmerman ;,; $26.66 ;,; 
-red   ;,;09/15/17 ,Guadalupe Freeman ;,; $25.95;,; 
-green&red ;,;   09/15/17   ,Irving Patterson 
-;,;$19.55 ;,; green&white&red ;,;   09/15/17 ,Karl Ross;,;   $15.68;,;   white ;,;   09/15/17 , Brandy Cortez ;,;$23.57;,;   white&red   ;,;09/15/17, 
-Mamie Riley   ;,;$29.32;,; purple;,;09/15/17 ,Mike Thornton   ;,; $26.44 ;,;   purple   ;,; 09/15/17, 
-Jamie Vaughn   ;,; $17.24;,;green ;,; 09/15/17   , 
-Noah Day ;,;   $8.49   ;,;green   ;,;09/15/17   
-,Josephine Keller ;,;$13.10 ;,;green;,;   09/15/17 ,   Tracey Wolfe;,;$20.39 ;,; red   ;,; 09/15/17 ,
-Ignacio Parks;,;$14.70   ;,; white&red ;,;09/15/17 
-, Beatrice Newman ;,;$22.45   ;,;white&purple&red 
+rood   ;,;09/15/17 ,Guadalupe Freeman ;,; $25.95;,; 
+groen&rood ;,;   09/15/17   ,Irving Patterson 
+;,;$19.55 ;,; groen&wit&rood ;,;   09/15/17 ,Karl Ross;,;   $15.68;,;   wit ;,;   09/15/17 , Brandy Cortez ;,;$23.57;,;   wit&rood   ;,;09/15/17, 
+Mamie Riley   ;,;$29.32;,; paars;,;09/15/17 ,Mike Thornton   ;,; $26.44 ;,;   paars   ;,; 09/15/17, 
+Jamie Vaughn   ;,; $17.24;,;groen ;,; 09/15/17   , 
+Noah Day ;,;   $8.49   ;,;groen   ;,;09/15/17   
+,Josephine Keller ;,;$13.10 ;,;groen;,;   09/15/17 ,   Tracey Wolfe;,;$20.39 ;,; rood   ;,; 09/15/17 ,
+Ignacio Parks;,;$14.70   ;,; wit&rood ;,;09/15/17 
+, Beatrice Newman ;,;$22.45   ;,;wit&paars&rood 
 ;,;   09/15/17, Andre Norris   ;,;   $28.46   ;,;   
-red;,;   09/15/17 ,   Albert Lewis ;,; $23.89;,;   
-black&red;,; 09/15/17,   Javier Bailey   ;,;   
-$24.49   ;,; black&red ;,; 09/15/17   , Everett Lyons ;,;$1.81;,;   black&red ;,; 09/15/17 ,   
-Abraham Maxwell;,; $6.81   ;,;green;,;   09/15/17   
-,   Traci Craig ;,;$0.65;,; green&yellow;,; 
+rood;,;   09/15/17 ,   Albert Lewis ;,; $23.89;,;   
+zwart&rood;,; 09/15/17,   Javier Bailey   ;,;   
+$24.49   ;,; zwart&rood ;,; 09/15/17   , Everett Lyons ;,;$1.81;,;   zwart&rood ;,; 09/15/17 ,   
+Abraham Maxwell;,; $6.81   ;,;groen;,;   09/15/17   
+,   Traci Craig ;,;$0.65;,; groen&geel;,; 
 09/15/17 , Jeffrey Jenkins   ;,;$26.45;,; 
-green&yellow&blue   ;,;   09/15/17,   Merle Wilson 
-;,;   $7.69 ;,; purple;,; 09/15/17,Janis Franklin   
-;,;$8.74   ;,; purple&black   ;,;09/15/17 ,  
-Leonard Guerrero ;,;   $1.86   ;,;yellow  
-;,;09/15/17,Lana Sanchez;,;$14.75   ;,; yellow;,;   
+groen&geel&blauw   ;,;   09/15/17,   Merle Wilson 
+;,;   $7.69 ;,; paars;,; 09/15/17,Janis Franklin   
+;,;$8.74   ;,; paars&zwart   ;,;09/15/17 ,  
+Leonard Guerrero ;,;   $1.86   ;,;geel  
+;,;09/15/17,Lana Sanchez;,;$14.75   ;,; geel;,;   
 09/15/17   ,Donna Ball ;,; $28.10  ;,; 
-yellow&blue;,;   09/15/17   , Terrell Barber   ;,; 
-$9.91   ;,; green ;,;09/15/17   ,Jody Flores;,; 
-$16.34 ;,; green ;,;   09/15/17,   Daryl Herrera 
-;,;$27.57;,; white;,;   09/15/17   , Miguel Mcguire;,;$5.25;,; white&blue   ;,;   09/15/17 ,   
-Rogelio Gonzalez;,; $9.51;,;   white&black&blue   
+geel&blauw;,;   09/15/17   , Terrell Barber   ;,; 
+$9.91   ;,; groen ;,;09/15/17   ,Jody Flores;,; 
+$16.34 ;,; groen ;,;   09/15/17,   Daryl Herrera 
+;,;$27.57;,; wit;,;   09/15/17   , Miguel Mcguire;,;$5.25;,; wit&blauw   ;,;   09/15/17 ,   
+Rogelio Gonzalez;,; $9.51;,;   wit&zwart&blauw   
 ;,;   09/15/17   ,   Lora Hammond ;,;$20.56 ;,; 
-green;,;   09/15/17,Owen Ward;,; $21.64   ;,;   
-green&yellow;,;09/15/17,Malcolm Morales ;,;   
-$24.99   ;,;   green&yellow&black;,; 09/15/17 ,   
-Eric Mcdaniel ;,;$29.70;,; green ;,; 09/15/17 
-,Madeline Estrada;,;   $15.52;,;green;,;   09/15/17 
-, Leticia Manning;,;$15.70 ;,; green&purple;,; 
-09/15/17 ,   Mario Wallace ;,; $12.36 ;,;green ;,; 
+groen;,;   09/15/17,Owen Ward;,; $21.64   ;,;   
+groen&geel;,;09/15/17,Malcolm Morales ;,;   
+$24.99   ;,;   groen&geel&zwart;,; 09/15/17 ,   
+Eric Mcdaniel ;,;$29.70;,; groen ;,; 09/15/17 
+,Madeline Estrada;,;   $15.52;,;groen;,;   09/15/17 
+, Leticia Manning;,;$15.70 ;,; groen&paars;,; 
+09/15/17 ,   Mario Wallace ;,; $12.36 ;,;groen ;,; 
 09/15/17,Lewis Glover;,;   $13.66   ;,;   
-green&white;,;09/15/17,   Gail Phelps   ;,;$30.52   
-;,; green&white&blue   ;,; 09/15/17 , Myrtle Morris 
-;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
+groen&wit;,;09/15/17,   Gail Phelps   ;,;$30.52   
+;,; groen&wit&blauw   ;,; 09/15/17 , Myrtle Morris 
+;,;   $22.66   ;,; groen&wit&blauw;,;09/15/17"""
 
 dagelijkse_verkoop_vervangen = dagelijkse_verkoop.replace(";,;", ";")
 
