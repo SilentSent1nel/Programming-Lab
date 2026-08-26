c805abe9516f5286c5f22f3ea3260713536c5055
Mirkan-23 <mirkan070@outlook.com>
2026-08-26 21:27:26 +0200

    Oefening 12

---
+++ Python/07-strings/oefening12.py
@@ -0,0 +1,10 @@
+gedicht_titel = "spring storm"
+gedicht_auteur = "William Carlos Williams"
+
+gedicht_titel_gecorrigeerd = gedicht_titel.title()
+gedicht_auteur_gecorrigeerd = gedicht_auteur.upper()
+
+print(gedicht_titel)
+print(gedicht_titel_gecorrigeerd)
+print(gedicht_auteur)
+print(gedicht_auteur_gecorrigeerd)
