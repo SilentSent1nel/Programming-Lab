2f60df70338727fc949db759eaca1163302b8f1f
Mirkan-23 <mirkan070@outlook.com>
2026-08-26 22:55:05 +0200

    Oefening 21

---
+++ Python/07-strings/oefening21.py
@@ -0,0 +1,4 @@
+def gedicht_titelkaart(titel, dichter):
+    return "Het gedicht {} is geschreven door {}".format(titel, dichter)
+
+print(gedicht_titelkaart("I Hear America Singing", "Walt Whitman"))
