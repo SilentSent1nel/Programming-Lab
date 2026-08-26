b153725ff3f793c58dbbc126861561d13a54611c
Mirkan-23 <mirkan070@outlook.com>
2026-08-26 02:13:28 +0200

    Oefening 5

---
+++ Python/07-strings/oefening5.py
@@ -0,0 +1,5 @@
+bedrijfsmotto = "Copeland's Corporate Company helpt u op bekwame wijze om te gaan met de constante kakofonie van het dagelijks leven"
+
+een_na_laatste = bedrijfsmotto[-2]
+laatste_woord = bedrijfsmotto[-4:]
+print(laatste_woord)
