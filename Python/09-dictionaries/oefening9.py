6aea7ac285a45578faae32b4b2d74a1a66c52898
Mirkan-23 <mirkan070@outlook.com>
2026-09-08 18:10:03 +0200

    Oefening 9

---
+++ Python/09-dictionaries/oefening9.py
@@ -0,0 +1,5 @@
+liedjes = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+keren_afgespeeld = [78, 29, 44, 21, 89, 5]
+
+gespeeld = {key:value for key, value in zip(liedjes, keren_afgespeeld)}
+print(gespeeld)
