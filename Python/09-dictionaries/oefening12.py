7bc8b43baabce01345dfa079bf13166aa933bc90
Mirkan-23 <mirkan070@outlook.com>
2026-09-08 18:57:43 +0200

    Oefening 12

---
+++ Python/09-dictionaries/oefening12.py
@@ -0,0 +1,6 @@
+dierenriem_elementen = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+dierenriem_elementen["energie"] = "Geen dierenriem element"
+
+if "energie" in dierenriem_elementen:
+    print(dierenriem_elementen)
