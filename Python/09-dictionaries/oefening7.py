0c974fe856a77a28512d6dd769c967ed858e6a7f
Mirkan-23 <mirkan070@outlook.com>
2026-09-08 18:00:18 +0200

    Oefening 7

---
+++ Python/09-dictionaries/oefening7.py
@@ -0,0 +1,6 @@
+oscar_winnaars = {"Best foto": "La La Land", "Beste acteur": "Casey Affleck", "Best actrice": "Emma Stone", "Geanimeerde functie": "Zootopia"}
+
+oscar_winnaars["Bijrol actrice"] = "Viola Davis"
+oscar_winnaars["Beste foto"] = "Moonlight"
+
+print(oscar_winnaars)
