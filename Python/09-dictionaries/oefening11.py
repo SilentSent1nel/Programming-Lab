f10e6e2c0ffed0e12413c72b205bf23a7f73dd3c
Mirkan-23 <mirkan070@outlook.com>
2026-09-08 18:50:49 +0200

    Oefening 11

---
+++ Python/09-dictionaries/oefening11.py
@@ -0,0 +1,4 @@
+dierenriem_elementen = {"water": ["Cancer", "Scorpio", "Pisces"], "vuur": ["Aries", "Leo", "Sagittarius"], "aarde": ["Taurus", "Virgo", "Capricorn"], "lucht":["Gemini", "Libra", "Aquarius"]}
+
+print(dierenriem_elementen["aarde"])
+print(dierenriem_elementen["vuur"])
