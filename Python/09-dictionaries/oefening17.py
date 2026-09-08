5d10186927981628774a75c1549d76333f70625a
Mirkan-23 <mirkan070@outlook.com>
2026-09-08 20:28:18 +0200

    Oefening 17

---
+++ Python/09-dictionaries/oefening17.py
@@ -0,0 +1,4 @@
+percentage_vrouwen_in_beroep = {"CEO": 28, "Technisch Manager": 9, "Apotheker": 58, "Arts": 40, "Advocaat": 37, "Luchtvaartingenieur": 9}
+
+for key, value in percentage_vrouwen_in_beroep.items():
+    print(f"Vrouwen vormen {value}% van {key}")
