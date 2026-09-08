0517a0f8484b82550baa447676398e32119fbfe4
Mirkan-23 <mirkan070@outlook.com>
2026-09-08 18:18:12 +0200

    Oefening 9

--- Python/09-dictionaries/oefening9.py
+++ Python/09-dictionaries/oefening9.py
@@ -2,4 +2,13 @@ liedjes = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On",
 keren_afgespeeld = [78, 29, 44, 21, 89, 5]
 
 gespeeld = {key:value for key, value in zip(liedjes, keren_afgespeeld)}
+
 print(gespeeld)
+
+gespeeld["Purple Haze"] = 1
+gespeeld.update({"Respect": 94})
+
+print(gespeeld)
+
+bibliotheek = {"De beste liedjes": gespeeld, "Sunday Feelings": {}}
+print(bibliotheek)
